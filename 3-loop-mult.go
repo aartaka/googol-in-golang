37c6a22726723add74242b812292bcecede74bfa
Artyom Bologov <aartaka@protonmail.com>
2021-05-19 16:16:49 +0500

    3-loop-mult: Use increment and shorter initialization.

--- 3-loop-mult.go
+++ 3-loop-mult.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var googol float64 = 1
-	for i := 0; i < 100; i += 1 {
+	googol := 1.0
+	for i := 0; i < 100; i++ {
 		googol *= 10
 	}
 	fmt.Printf("%e\n", googol)
